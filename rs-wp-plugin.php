0c05fe798b7ed839a5e0c03b1c98dd087f984c67
Md Mozahidur Rahman <mr.rousnay@gmail.com>
2022-04-13 16:19:23 +0600

    Plugin file header added

--- rs-wp-plugin.php
+++ rs-wp-plugin.php
@@ -1,1 +1,10 @@
-
+<?php
+/*
+Plugin Name: RS WP Plugin
+Description: Multipurpose WordPress plugin development.
+Plugin URI:  https://wp.wittyplex.com/plugins
+Author:      Mozahidur Rahman Rousnay
+Version:     1.0
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
